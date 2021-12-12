2a3a67044a1303a1fcf40eec7a652e1cc4e66d90
Markopolo619 <markmadi123@protonmail.com>
2021-12-12 18:21:01 +0200

    Finished

--- src/Components/Content.js
+++ src/Components/Content.js
@@ -1,11 +1,39 @@
-const Content = () => {
-  return (
-    <main className="flex-grow">
-      <div className="py-32 text-center">
-        <h2 className="font-extrabold text-6xl">Navbars in Tailwind</h2>
-      </div>
-    </main>
-  );
-};
+import { useState, useEffect } from "react";
+import axios from "axios";
 
-export default Content;
+const Content = () => {
+const [error, setError] = useState(null);
+const [isLoaded, setIsLoaded] = useState(false);
+const [facts, setFacts] = useState([]);
+
+useEffect(() => {
+  fetch("https://numbersapi.p.rapidapi.com/random/trivia")
+    .then(res => res.json())
+    .then(
+      (result) => {
+        setIsLoaded(true);
+        setFacts(result);
+      },(error) => {
+        setIsLoaded(true);
+        setError(error);
+      }
+    )
+}, []);
+
+if (error) {
+  return <div>Error: {error.message}</div>
+}else if(!isLoaded){
+  return <div>Loading...</div>;
+}else{
+  return (
+    <main>
+      <ul>
+        {facts.map(facts => (<li key={facts}>{facts}</li>))}
+      </ul>
+    </main>
+  )
+}
+}
+
+export default Content
+
--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -6,20 +6,20 @@ const Nav = () => {
           <div className="flex space-x-4">
             <div>
               <a
-                href=""
-                class="flex items-center py-5 px-2 text-gray-700 hover:text-gray-900"
+                href="#"
+                className="flex items-center py-5 px-2 text-gray-700 hover:text-gray-900"
               >
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
-                  class="h-6 w-6"
+                  className="h-6 w-6"
                   fill="none"
                   viewBox="0 0 24 24"
                   stroke="currentColor"
                 >
                   <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
                     d="M14.828 14.828a4 4 0 01-5.656 0M9 10h.01M15 10h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
                   />
                 </svg>
