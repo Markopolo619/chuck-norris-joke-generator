2c8b57c95e78e882db0b71fc85bac464963b67bd
Markopolo619 <markmadi123@protonmail.com>
2021-12-12 20:07:43 +0200

    Changed Page Title and logo

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Nav from './Components/Nav';
 import Content from './Components/Content';
 import Footer from './Components/Footer';
+import ChangePageTitle from './Components/ChangePageTitle';
 
 function App() {
   return (
     <div className="App flex flex-col min-h-screen">
+      <ChangePageTitle pageTitle="Chuck Norris Jokes" />
       <Nav />
       <Content />
       <Footer />
---
+++ src/Components/ChangePageTitle.js
@@ -0,0 +1,14 @@
+import {useEffect } from 'react';
+
+const ChangePageTitle = ({ pageTitle}) => {
+    useEffect(() => {
+        const prevTitle = document.title;
+        document.title = pageTitle;
+        return () => {
+            document.title = prevTitle;
+        }
+    })
+    return <></>;
+}
+
+export default ChangePageTitle
