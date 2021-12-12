642a35702ffe10f7f726d60faa49679bf9b9c335
Markopolo619 <markmadi123@protonmail.com>
2021-12-12 21:17:23 +0200

    Added a refresh button and rewrote the readme

--- src/Components/Content.js
+++ src/Components/Content.js
@@ -1,5 +1,5 @@
-import { useState, useEffect } from 'react';
-import axios from 'axios';
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 //Content component
 const Content = () => {
@@ -7,17 +7,30 @@ const Content = () => {
   const [data, setData] = useState([]);
 
   useEffect(() => {
-    axios.get('https://api.chucknorris.io/jokes/random').then(res => {
+    axios.get("https://api.chucknorris.io/jokes/random").then((res) => {
       setData(res.data);
     });
+  }, []);
+
+  function refreshPage() {
+    window.location.reload(false);
   }
-  , []);
 
   return (
     <main className="flex-grow">
       <p className="text-center px-2 pt-44 text-2xl">{data.value}</p>
+      <div className="button flex items-center">
+        <div className="button2">
+          <button
+            class="bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded"
+            onClick={refreshPage}
+          >
+            Click for a new joke
+          </button>
+        </div>
+      </div>
     </main>
   );
-}
+};
 
 export default Content;
