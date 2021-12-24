234ddad60b6f031227c832d10076bf89b549684a
Mark <markmadi232@gmail.com>
2021-12-24 12:35:28 +0200

    Tried to fix the button

--- src/Components/Content.js
+++ src/Components/Content.js
@@ -20,7 +20,7 @@ const Content = () => {
     <main className="flex-grow">
       <p className="text-center px-2 pt-44 text-2xl">{data.value}</p>
       <div className="button flex items-center">
-        <div className="button2">
+        <div className="button2 flex justify-center">
           <button
             class="bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded"
             onClick={refreshPage}
